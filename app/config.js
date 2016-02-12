c162365a3318a70252a3ff6f1e2a036593090fdb
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 19:19:20 -0800

    Increment visits counter in mongodb. Clean up code

--- app/config.js
+++ app/config.js
@@ -38,7 +38,6 @@ userSchema.pre('save', function(next) {
   return cipher(this.password, null, null).bind(this)
     .then(function(hash) {
       this.password = hash;
-      console.log('hashed pword', hash);
       next();
     });
 });
